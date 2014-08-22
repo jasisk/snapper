7bab9969b4f2dede4339c9253ba62e0ff4fdaaab
Jean-Charles Sisk <jasisk@gmail.com>
2014-08-22 02:24:50 -0400

    moving error handler middleware

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ var express = require('express');
 var webhook = require('./lib/webhook');
 var app = express();
 
+function errorHandler(err, req, res, next) {
+  console.dir(err, err.stack);
+  return res.status(500).send('unknown error');
+}
+
 app.disable('x-powered-by');
 
+app.use(errorHandler);
 app.use('/hook', webhook());
 
 app.use('*', function (req, res) {
